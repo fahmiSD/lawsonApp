16b0a15a1b4dc0c3d6a894fe891eb7cef5620c5f
Fahmi Sabila Dinnulhaq <fahmisabiladinnulhaq@gmail.com>
2023-05-28 02:37:03 +0700

    sql

---
+++ lawson.sql
@@ -0,0 +1,26 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: May 27, 2023 at 09:33 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `lawson`
+--
+CREATE DATABASE IF NOT EXISTS `lawson` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `lawson`;
+
+-- -------------------------------------
